09542595c60d3ad588f3122ca079d1313e591abe
Iveta Staune <iveta.staune@gmail.com>
2022-02-01 15:02:39 +0200

    .

--- src/api_key.ts
+++ src/api_key.ts
@@ -1,8 +1,6 @@
 const api = {
   baseURI: 'https://api.openweathermap.org/data/2.5/weather',
   mapsStylesUrl: 'mapbox://styles/ywette/ckz2z573a008514ltupffrrsj',
-  REACT_APP_WEATHER_API_KEY: '52a6474309560c40a15cad2c2650195b',
-  REACT_APP_MAP_API_KEY: 'pk.eyJ1IjoieXdldHRlIiwiYSI6ImNreXo0Ynd1ZTA0ZHoycG1pcGM1a3AybTcifQ.zjgHL52rdTgs2FNG2ZYEOA',
 };
 
 export default api;
